abfd3bd1e0aac159155f2cf5ff9e505575727743
Angus <anguswwilliams@outlook.com>
2026-03-16 19:53:49 -0700

    fairly sure I just fixed most of the buggs

--- projects/game/game.js
+++ projects/game/game.js
@@ -2,6 +2,138 @@ let gameActive = true;
 
 function classroom() {
     clear();
+    print("\nAfter the earthquake you were trapped inside of Brickston High School and the front entrance caved in. Your objective is to navigate through the rooms and try to escape. Good luck!");
+
+    print("\nWhere do you want to go?");
+    print("\n\tthe hallway");
+
+    function processInput(input){
+        if (input.toLowerCase() === "hallway") {
+            hallway();
+        } else {
+            stayHere();
+            waitThenCall(classroom);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function hallway() {
+    clear();
+    print("\nYou exit the room and find yourself in a dark hallway. A faint shaft of light peeks from the broken roof and you see several open doors.");
+
+    print("\nWhere do you want to go next?");
+    print("\n\tlibrary");
+    print("\n\tthe box");
+    print("\n\tcommons");
+
+    function processInput(input){
+        input = input.toLowerCase();
+
+        if (input === "library") {
+            library();
+        } else if (input === "the box") {
+            thebox();
+        } else if (input === "commons") {
+            commons();
+        } else {
+            stayHere();
+            waitThenCall(hallway);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function library() {
+    clear();
+    print("\nYou enter the library.");
+    print("\nThe library is on fire and all the books are burning. A golden key lies in the corner.");
+
+    print("\nWhere do you want to go next?");
+    print("\n\tscience room");
+    print("\n\thallway");
+
+    function processInput(input){
+        input = input.toLowerCase();
+
+        if (input === "hallway") {
+            hallway();
+        } else if (input === "science room") {
+            scienceroom();
+        } else {
+            stayHere();
+            waitThenCall(library);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function commons() {
+    clear();
+    print("\nAll the tables are flipped over and backpacks are everywhere. It looks like there is no escape here.");
+
+    print("\nWhere do you want to go next?");
+    print("\n\tlibrary");
+
+    function processInput(input){
+        if (input.toLowerCase() === "library") {
+            library();
+        } else {
+            stayHere();
+            waitThenCall(commons);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function scienceroom() {
+    clear();
+    print("\nYou enter the science room. A chemical fire is burning.");
+
+    print("\nWhere do you want to go next?");
+    print("\n\tlibrary");
+
+    function processInput(input){
+        if (input.toLowerCase() === "library") {
+            library();
+        } else {
+            stayHere();
+            waitThenCall(scienceroom);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function thebox() {
+    clear();
+    print("\nYou walk into the gym (The Box).");
+
+    print("\nWhere do you want to go next?");
+    print("\n\thallway");
+
+    function processInput(input){
+        if (input.toLowerCase() === "hallway") {
+            hallway();
+        } else {
+            stayHere();
+            waitThenCall(thebox);
+        }
+    }
+    waitForInput(processInput);
+}
+
+function start(){
+    print("Welcome to my game! Press any key to start.");
+
+    function processInput(input){
+        classroom();
+    }
+
+    waitForInput(processInput);
+}let gameActive = true; 
+
+function classroom() {
+    clear();
     print("\nAfter the earthquake you were trapped inside of Brickston High School and the front entrance caved in, your objective is to navigate through the rooms and try to escape. Good luck!");
 
     print("\nWhere do you want to go?");
@@ -123,9 +255,6 @@ function escape() {
     gameActive = false;
 }
 
-//finally, make sure you customize this to tell it what should happen at the
-//very start. For this simple example, any input will bring you
-//to locationA
 function start(){
     print("Welcome to my game! Press any key to start");
 
