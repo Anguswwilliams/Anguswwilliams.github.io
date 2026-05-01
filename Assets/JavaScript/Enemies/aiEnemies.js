36a5a70620a31879e41471f6d6f042eb6e5f8455
Angus <anguswwilliams@outlook.com>
2026-05-01 08:55:26 -0400

    Update import paths in aiEnemies.js

---
+++ Assets/JavaScript/Enemies/aiEnemies.js
@@ -0,0 +1,7 @@
+// aiEnemies.js
+
+import { someFunction } from '../World/main.js';
+import { anotherFunction } from '../World/plains.js';
+import { baseFunction } from './base.js';
+
+// Your other code here...
