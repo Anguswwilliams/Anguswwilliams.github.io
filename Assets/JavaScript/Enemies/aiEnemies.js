c44fc7b48bd8da5fda3a8bb2b7a683ce78e747f2
Angus <anguswwilliams@outlook.com>
2026-05-05 08:04:24 -0400

    Delete Assets/JavaScript/Enemies directory

--- Assets/JavaScript/Enemies/aiEnemies.js
+++
@@ -1,7 +0,0 @@
-// aiEnemies.js
-
-import { someFunction } from '../World/main.js';
-import { anotherFunction } from '../World/plains.js';
-import { baseFunction } from './base.js';
-
-// Your other code here...
